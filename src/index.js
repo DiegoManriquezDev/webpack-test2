650604a24bfb7c902975b26ef256e5e543f6bf10
Diegodev <diegodev.manriquez@gmail.com>
2022-05-03 15:59:52 -0400

    webpack test2

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
+import Notes from './data.csv';
+
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack', 'Diego'], ' ');
+    element.classList.add('hello');
+
+
+    //Add the image to aour existing div
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+
+    console.log(Data);
+    console.log(Notes);
+
+
+    return element;
+  }
+  
+  document.body.appendChild(component());
